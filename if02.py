a7d4b5524100d39c09187d54903ab5fbbcc207cb
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-19 11:56:38 +0500

    :bajarildi

--- if02.py
+++ if02.py
@@ -1,11 +1,8 @@
 def main(a,b,c):
-    """
-    Find the smallest of the numbers.
-    Args:
-        a: First number.
-        b: Second number.
-        c: Third number.
-    Returns:
-        int: return answer.
-    """
-    return
+    if a>=0 and a<=9 and a<b and a<c:
+        print('a eng kichik raqam')
+    if b>=0 and b<=9 and b<a and b<c:
+        print('b eng kichik raqam')
+    if c>=0 and c<=9 and c<a and c<b:
+        print('c eng kichik raqam')
+print(main(7,25,2))
